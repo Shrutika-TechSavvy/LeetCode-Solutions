ce5bc7c5850fd2dc86136151f096581940fd5627
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2024-08-23 10:24:51 +0530

    Time: 8 ms (45.87%), Space: 41.5 MB (92.66%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -1,69 +1,30 @@
 class Solution {
-    public String fractionAddition(String expression) {
-        int n = expression.length();
-        int c = 0;
-        for(int i=0;i<n;i++){
-            if(expression.charAt(i)=='/'){
-                c++;
-            }
+    private int gcd(int a,int b)
+    {
+        while(b!=0)
+        {
+            int temp=b;
+            b=a%b;
+            a=temp;
         }
-        int[] num = new int[c];
-        int[] den = new int[c];
-        int[] s = new int[c];
-        int i = 0;
-        if(expression.charAt(0)=='-'){
-            s[0]=-1;
-            i=1;
-        }
-        else{
-            s[0]=1;
-        }
-        int j=0,k=0,l=1;
-        while (i < n) {
-            if (expression.charAt(i) == '-' || expression.charAt(i) == '+') {
-                if (expression.charAt(i) == '-') {
-                    s[l] = -1;
-                } else {
-                    s[l] = 1;
-                }
-                l++;
-                i++;
-            }
-            int nu=0;
-            while (i < n && Character.isDigit(expression.charAt(i))) {
-                nu=nu*10+(expression.charAt(i)-'0');
-                i++;
-            }
-            num[j++] = nu;
-            i++; 
-            int de = 0;
-            while (i < n && Character.isDigit(expression.charAt(i))) {
-                de=de* 10+(expression.charAt(i)-'0');
-                i++;
-            }
-            den[k++]=de;
-        }
-        int lcm = den[0];
-        for(i=1;i<c;i++){
-            lcm = lcm*(den[i]/helper(lcm,den[i]));;
-        }
-        for(i=0;i<c;i++){
-            int a = (lcm/den[i])*s[i];
-            num[i]*=a;
-        }
-        int sum=0;
-        for(i=0;i<c;i++){
-            sum+=num[i];
-        }
-        int x = helper(Math.abs(sum),lcm);
-        sum=sum/x;
-        lcm=lcm/x;
-        String str = Integer.toString(sum)+"/"+Integer.toString(lcm);
-        return str;
+        return a;
     }
-    public static int helper(int a,int b) {
-        if (b==0)
-            return a;
-        return helper(b, a % b);
+    public String fractionAddition(String expression) {
+       String[] fractions=expression.split("(?=[+-])");
+       int numerator=0,denominator=1;
+       for(String frac:fractions)
+       {
+        String[] parts=frac.split("/");
+        int num=Integer.parseInt(parts[0]);
+        int denom=Integer.parseInt(parts[1]);
+        numerator=numerator*denom+num*denominator;
+        denominator*=denom;
+
+        int gcd=gcd(Math.abs(numerator),denominator);
+
+        numerator/=gcd;
+        denominator/=gcd;
+       }
+       return numerator+"/"+denominator;
     }
 }
