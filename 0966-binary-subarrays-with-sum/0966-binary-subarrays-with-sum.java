7ffb16ff7142892b749dbaab678301cda7b34ca2
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2024-12-09 18:29:41 +0530

    Time: 1834 ms (5.07%), Space: 48 MB (35.73%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,17 +1,34 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
-    }
-    private int atMost(int[] nums, int goal) {
-        int head, tail = 0, sum = 0, result = 0;
-        for (head = 0; head < nums.length; head++) {
-            sum += nums[head];
-            while (sum > goal && tail <= head) {
-                sum -= nums[tail];
-                tail++;
+        // return atMost(nums, goal) - atMost(nums, goal - 1);
+
+        int count = 0,sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            sum=0;
+            for (int j = i; j < nums.length; j++) {
+                // Calculate the sum of the subarray nums[i...j]
+                sum += nums[j];
+
+                // Check if the sum equals the goal
+                if (sum == goal) {
+                    count++;
+                }
             }
-            result += head - tail + 1;
         }
-        return result;
+        return count;
     }
+    /*
+     * private int atMost(int[] nums, int goal) {
+     * int head, tail = 0, sum = 0, result = 0;
+     * for (head = 0; head < nums.length; head++) {
+     * sum += nums[head];
+     * while (sum > goal && tail <= head) {
+     * sum -= nums[tail];
+     * tail++;
+     * }
+     * result += head - tail + 1;
+     * }
+     * return result;
+     * }
+     */
 }
