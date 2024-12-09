cbefaf174c357a447bf40292e6387eec9821fb4d
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2024-12-09 13:24:41 +0530

    Time: 17 ms (38.4%), Space: 42.6 MB (22.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,28 +1,36 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        //Condition: If both the strings don't have the same langth, it's not isomorphic
-        if(s.length()!=t.length())
-        {
+        // Condition: If both the strings don't have the same langth, it's not
+        // isomorphic
+        if (s.length() != t.length()) {
             return false;
         }
 
-        //Data structure preferred for mapping each character with each other is HashMap
-        HashMap<Character, Character> map=new HashMap<>();
-        
-        for(int i=0;i<s.length();i++)
-        {
-            char a=s.charAt(i);
-            char b=t.charAt(i);
-            if(map.containsKey(a)){
-                if(map.get(a).equals(b))
-                continue;
-                else
-                return false;
+        // Data structure preferred for mapping each character with each other is
+        // HashMap
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            // Forward mapping check (s -> t)
+            if (mapST.containsKey(charS)) {
+                if (mapST.get(charS) != charT) {
+                    return false;
+                }
+            } else {
+                mapST.put(charS, charT);
             }
-            else{
-                if(!map.containsValue(b))
-                    map.put(a,b);
-                    else return false;
+
+            // Reverse mapping check (t -> s)
+            if (mapTS.containsKey(charT)) {
+                if (mapTS.get(charT) != charS) {
+                    return false;
+                }
+            } else {
+                mapTS.put(charT, charS);
             }
         }
         return true;
