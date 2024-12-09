6af07c76e94626a4a160556703500a8f04f315fa
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2024-12-09 13:50:22 +0530

    Time: 8 ms (36.41%), Space: 42.6 MB (95.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,12 +2,32 @@ import java.util.Arrays;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
+
+        //Chcking of the both length are sam or not.If not, then they are not anagrams
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        //Frequency array 
+        int charCount[]=new int[26];
+
+        //At last the frequency array should have all value equal to zero because 
+        //if found in s,increment it and if found in t decrement it
+        for(int i=0;i< s.length();i++)
+        {
+            charCount[s.charAt(i)-'a']++; //incremeting the character count in frequency array
+            charCount[t.charAt(i)-'a']--; //Decremnting that in frquency arra
+        }
+
+
+//checking if any value if no-zero or nott.if found, the strings are not anagrams
+        for(int count:charCount)
+        {
+            if(count!=0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
