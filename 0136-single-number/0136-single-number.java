1b48b44d773a677d2d4b7ebad0e724d327ece07a
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2024-12-14 22:16:22 +0530

    Time: 1 ms (99.86%), Space: 46.7 MB (9.4%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int element=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            element^=nums[i];
+        }
+        return element;
+    }
+}
