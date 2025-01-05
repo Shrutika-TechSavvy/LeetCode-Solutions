acaff0ed900f3c94191c951bb8881630ec4826fb
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-01-05 21:09:05 +0530

    Time: 3 ms (86.59%), Space: 49.5 MB (56.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     private void bfs(char[][] grid, int i, int j, boolean[][] visited) {
         int[] dx = {0, 0, 1, -1};
         int[] dy = {1, -1, 0, 0};
@@ -37,6 +37,46 @@ class Solution {
                 if (grid[i][j] == '1' && !visited[i][j]) {
                     countIslands++;
                     bfs(grid, i, j, visited);
+                }
+            }
+        }
+        return countIslands;
+    }
+}
+*/
+
+
+class Solution {
+    private void dfs(char[][] grid, int i, int j, boolean[][] visited) {
+        // Boundary check and water check
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || 
+            grid[i][j] == '0' || visited[i][j]) {
+            return; // Stop the recursion if out of bounds, water, or already visited
+        }
+
+        // Mark the current land cell as visited
+        visited[i][j] = true;
+
+        // Explore all 4 directions without dx and dy arrays
+        dfs(grid, i + 1, j, visited); // Down
+        dfs(grid, i - 1, j, visited); // Up
+        dfs(grid, i, j + 1, visited); // Right
+        dfs(grid, i, j - 1, visited); // Left
+    }
+
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int countIslands = 0;
+
+        // Traverse the entire grid
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // If land and not visited, start DFS
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    countIslands++;
+                    dfs(grid, i, j, visited); // Explore the entire island
                 }
             }
         }
