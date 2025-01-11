a3bbe509470c70fd4806bafb6dc0253a349fe6b4
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-01-11 15:16:36 +0530

    Time: 7 ms (7.85%), Space: 43.1 MB (72.17%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public boolean isValid(StringBuilder curr) {
+        int balance = 0;
+        for (int i = 0; i < curr.length(); i++) {
+            if (curr.charAt(i) == '(') {
+                balance++;
+            } else {
+                balance--;
+            }
+            // If balance goes negative, there are more ')' than '('
+            if (balance < 0) {
+                return false;
+            }
+        }
+        // Should be zero if it's a valid parentheses string
+        return balance == 0;
+    }
+    public void solve(List<String> res, StringBuilder curr, int n){
+
+        if(curr.length()==2*n){
+            if(isValid(curr)){
+                res.add(curr.toString());
+            }
+            return;
+        }
+
+        curr.append("(");
+        solve(res, curr, n);
+       curr.deleteCharAt(curr.length() - 1);  // Backtrack
+
+        curr.append(")");
+        solve(res, curr, n);
+        curr.deleteCharAt(curr.length() - 1);  // Backtrack
+
+    }
+    public List<String> generateParenthesis(int n) {
+        
+        List<String> res=new ArrayList<>();
+        StringBuilder curr=new StringBuilder();
+        solve(res, curr, n);
+        return res;
+    }
+}
