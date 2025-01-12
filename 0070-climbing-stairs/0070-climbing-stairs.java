2282cca9523912cf297d2b2d8b751bfb392b1ef4
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-01-12 10:14:56 +0530

    Time: 0 ms (100%), Space: 40.9 MB (8.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    /*For memoization
     private int solve(int n, int[] dp){
         
         if(n<0) return 0;
@@ -11,6 +13,7 @@ class Solution {
         return dp[n]=solve(n-1, dp)+ solve(n-2,dp);
 
     }
+    */
     public int climbStairs(int n) {
        
        /*The recursive solution: TLE
@@ -20,9 +23,22 @@ class Solution {
         */
 
         //Trying the memoization
-        int dp[]=new int[n+1];
+        /*int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
         return solve(n, dp);
+        */
+
+        //trying out the tabulation method
+
+        if(n == 1) return 1;
+        int dp[]=new int[n+1];  
+        Arrays.fill(dp,-1); 
+        dp[0]=1; //way to stay at ground
+        dp[1]=1;
+        for(int i=2;i<=n;i++) {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
 
     }
 }
