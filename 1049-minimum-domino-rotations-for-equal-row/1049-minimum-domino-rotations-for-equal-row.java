c0f8e7f9580469bc927b40dcf613362261f5bd58
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-05-05 07:35:39 +0530

    Time: 6 ms (29.75%), Space: 50.2 MB (86.67%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -1,20 +1,57 @@
 class Solution {
-    private int getMinSwap(int[] tops, int[] bots, int target) {
-        int swapT = 0, swapB = 0;
-        for (int i = 0; i < tops.length; ++i) {
-            if (tops[i] != target && bots[i] != target) return -1;
-            if (tops[i] != target) ++swapT;
-            else if (bots[i] != target) ++swapB;
-        }
-        return Math.min(swapT, swapB);
-    }
     public int minDominoRotations(int[] tops, int[] bots) {
-        int minSwapA = getMinSwap(tops, bots, tops[0]);
-        int minSwapB = (tops[0] == bots[0]) ? -1 : getMinSwap(tops, bots, bots[0]);
+        int count1 = 0, count2 = 0, count3 = 0, count4 = 0;
 
-        if (minSwapA == -1 && minSwapB == -1) return -1;
-        if (minSwapB == -1) return minSwapA;
-        if (minSwapA == -1) return minSwapB;
-        return Math.min(minSwapA, minSwapB);
+        int num1 = tops[0];
+        int num2 = bots[0];
+
+        for (int i = 0; i < tops.length; i++) {
+            // count1: Make top all num1
+            if (count1 != Integer.MAX_VALUE) {
+                if (tops[i] == num1) {
+                    // do nothing
+                } else if (bots[i] == num1) {
+                    count1++;
+                } else {
+                    count1 = Integer.MAX_VALUE;
+                }
+            }
+
+            // count2: Make bottom all num1
+            if (count2 != Integer.MAX_VALUE) {
+                if (bots[i] == num1) {
+                    // do nothing
+                } else if (tops[i] == num1) {
+                    count2++;
+                } else {
+                    count2 = Integer.MAX_VALUE;
+                }
+            }
+
+            // count3: Make bottom all num2
+            if (count3 != Integer.MAX_VALUE) {
+                if (bots[i] == num2) {
+                    // do nothing
+                } else if (tops[i] == num2) {
+                    count3++;
+                } else {
+                    count3 = Integer.MAX_VALUE;
+                }
+            }
+
+            // count4: Make top all num2
+            if (count4 != Integer.MAX_VALUE) {
+                if (tops[i] == num2) {
+                    // do nothing
+                } else if (bots[i] == num2) {
+                    count4++;
+                } else {
+                    count4 = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int result = Math.min(Math.min(count1, count2), Math.min(count3, count4));
+        return result == Integer.MAX_VALUE ? -1 : result;
     }
 }
