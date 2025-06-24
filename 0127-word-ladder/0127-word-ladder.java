98afcac308d9e91bcfb861bcab2ed732702be9e2
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-06-24 19:38:03 +0530

    Time: 82 ms (57.51%), Space: 46.1 MB (77.66%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,64 +1,49 @@
 class Solution {
+
     class Pair{
         String first;
         int second;
 
-        public Pair(String _first, int _second){
-            this.first=_first;
-            this.second=_second;
+        Pair(String f, int s){
+            this.first = f;
+            this.second = s;
         }
     }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        
-        //The queue is required 
         Queue<Pair> queue=new LinkedList<>();
-
-        // Initialize BFS with the beginWord
         queue.offer(new Pair(beginWord, 1));
-
-
         Set<String> set=new HashSet<>();
-        int lengthOfWordlist=wordList.size();
+        int lengthOfWordList=wordList.size();
 
-
-        //Adding all the wordsin the set
-        for(int i=0; i<lengthOfWordlist; i++){
+        for(int i=0; i<lengthOfWordList; i++){
             set.add(wordList.get(i));
         }
 
-         if (!set.contains(endWord)) {
-            return 0;
-        }
-
+        if(!set.contains(endWord)) return 0;
         set.remove(beginWord);
 
         while(!queue.isEmpty()){
             String word=queue.peek().first;
             int steps=queue.peek().second;
-
             queue.poll();
-
             if(word.equals(endWord)) return steps;
 
-            //else we need to find
+            //else need to ifnd
             for(int i=0; i<word.length(); i++){
+                //for each character of word check for a to z combinations
 
-                //for each character of the word check for the a to z combinations
-                for(char ch='a'; ch<='z' ;ch++){
-                    char replacedCharArray[]=word.toCharArray();
-                    replacedCharArray[i]=ch;
-                    String replacedWord= new String(replacedCharArray);
-
-                    //If the newly created word is presented in set, then
+                for(char ch='a' ; ch<='z' ; ch++){
+                    char replacedCharArray[] = word.toCharArray();
+                    replacedCharArray[i]= ch;
+                    String replacedWord = new String(replacedCharArray);
+                    //if newly created word is presented in set, then
                     if(set.contains(replacedWord)){
-
-                        //remove it from the set
-                        set.remove(replacedWord);    // Remove to avoid re-visiting
-                        queue.offer(new Pair(replacedWord, steps+1));
+                        //remove it from set
+                        set.remove(replacedWord);  //remove to avoid revisiting
+                        queue.offer(new Pair(replacedWord ,steps+1));
                     }
                 }
             }
-
         }
 
         return 0;
