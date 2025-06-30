4343b27bd8a6a5972eb3231488454984e78a6474
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-06-30 10:36:02 +0530

    Time: 12 ms (99.45%), Space: 45.7 MB (48.6%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -6,8 +6,9 @@ class Solution {
         2. Go through the hashmap again,
              If the difference between two keys is exact 1, then add their frequencies and update longestHS
         3. Return longestHS
-        */
-
+        
+        //TC: O(N) + O(N)
+        //SC:O(N)
         HashMap<Integer, Integer> hm =new HashMap<>();
         for(int i : nums ){
             hm.put(i, hm.getOrDefault(i, 0) + 1);
@@ -21,5 +22,22 @@ class Solution {
 
         return longestHS;
 
+        */
+        //Optimal approach
+        Arrays.sort(nums);
+        int left = 0; 
+        int right = 1;
+        int n = nums.length;
+        int res = 0;
+
+        while( right < nums.length){
+            int diff = nums[right] - nums[left];
+            if(diff == 1 ){
+                res = Math.max(res, right-left+1);
+            }
+            if(diff <= 1) right++;
+            else left++ ;
+        }
+        return res;
     }
 }
