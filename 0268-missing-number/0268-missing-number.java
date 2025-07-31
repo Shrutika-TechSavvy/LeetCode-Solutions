7c2a32a1a62fc5eb687f7ce62eb98f528aa5e380
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-07-31 19:04:47 +0530

    Time: 0 ms (100%), Space: 45.2 MB (71.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,6 +3,10 @@ class Solution {
         //SImply calculate the sum of n natural numbers
         //iterate through the array and find the sum of all available numbers
         //return sum of n natural no. - sum of all available no. 
+
+        //This is one of the optimal approach 
+
+        /*
         int n=nums.length;
         int sum_of_all_n_numbers=n*(n+1)/2;
         int sum_of_no_array=0;
@@ -12,6 +16,20 @@ class Solution {
             sum_of_no_array+=nums[i];
         }
         return sum_of_all_n_numbers - sum_of_no_array;
+
+        */
+
+        //Optimal appraoch 2
+        //Preferred
+        int xor1 = 0, xor2 = 0;
+        int n = nums.length;
+        for(int i = 0; i < nums.length; i++){
+            xor2 = xor2 ^ nums[i];
+            xor1 = xor1 ^ (i+1);
+        }
+        //xor1 = xor1 ^ n;
+
+        return (xor1 ^ xor2);
 
     }
 }
