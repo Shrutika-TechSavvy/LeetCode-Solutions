bcc4baabbbaf4a6f1627ab22eee2766c2e372d53
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-08-12 18:13:19 +0530

    Time: 2 ms (93.92%), Space: 46.6 MB (68.48%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,3 +1,23 @@
+
+//this is teh optimal approach actually
+//The observation is too impressive
+
+class Solution {
+    public int[] countBits(int n) {
+        int result[] = new int[n  + 1 ];
+
+        for(int i = 1; i <= n ; i++){
+            if(i % 2 == 0) result[i]  = result[i / 2];
+            else result[i] = result[i / 2] + 1;
+        }
+        return result;
+        
+    }
+}
+
+/*
+Gives the brute force approach 
+
 class Solution {
 
     static int countSetBits(int n)
@@ -32,3 +52,5 @@ class Solution {
 
     }
 }
+
+*/
