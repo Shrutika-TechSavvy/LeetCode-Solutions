ad95eeebae79a6363a9c17c1c400488a55bac957
Shrutika Vasant Pawar <shrutikapawar951@gmail.com>
2025-09-25 08:00:53 +0530

    Time: 8 ms (16.37%), Space: 45.7 MB (30.46%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,17 @@
 class Solution {
-    int solve(int i, int j, int n, List<List<Integer>> t, int[][] dp){
-        if(i>=n || j>=t.get(i).size()) return (int)(1e9);
-        if(i==n-1) return t.get(i).get(j);
-
-        if(dp[i][j] != -1)return dp[i][j];
-        int down=t.get(i).get(j) + solve(i+1, j, n, t, dp);
-        int diagonal=t.get(i).get(j) + solve(i+1, j+1, n, t, dp);
-        return dp[i][j] =Math.min(down, diagonal);
-
-    }
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n=triangle.size();
-        int[][] dp=new int[n][n];
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
-        }
-        return solve(0, 0,n, triangle, dp);
-        
-    }
+ public int minimumTotal(List<List<Integer>> triangle) {
+     int n = triangle.size();
+     
+     for (int i = n - 2; i >= 0; i--) {
+         for (int j = 0; j <= i; j++) {
+             int curr = triangle.get(i).get(j);
+             int below = triangle.get(i + 1).get(j);
+             int belowRight = triangle.get(i + 1).get(j + 1);
+             
+             triangle.get(i).set(j, curr + Math.min(below, belowRight));
+         }
+     }
+     
+     return triangle.get(0).get(0);
+ }
 }
